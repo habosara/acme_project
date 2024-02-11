add01705e54432448023b799897c0f47c2e716b6
Saci <hs.sarahabo@gmail.com>
2024-02-11 19:46:59 +0100

    Dark mode button and function added

--- script.js
+++ script.js
@@ -1,15 +1,57 @@
-// Get the current page URL
 const currentPageUrl = window.location.pathname;
-console.log(currentPageUrl)
 
-// Find all menu items
 const menuItems = document.querySelectorAll('.menu-item');
 
-// Loop through each menu item
 menuItems.forEach(item => {
-    // Compare the href of each menu item with the current page URL
+
     if (item.getAttribute('href') === currentPageUrl) {
-        // If they match, add the 'active' class to the menu item
         item.classList.add('active');
     }
 });
+
+
+//DARK MODE
+
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+    if (localStorageTheme !== null) {
+        return localStorageTheme;
+    }
+
+    if (systemSettingDark.matches) {
+        return "dark";
+    }
+
+    return "light";
+}
+
+function updateButton({ buttonEl, isDark }) {
+    const newCta = isDark ? "light_mode" : "dark_mode";
+    const ctaText = isDark ? "" : ""
+
+    buttonEl.innerHTML = `<span class="material-symbols-outlined">${newCta}</span>${ctaText}`;
+
+    buttonEl.setAttribute("aria-label", isDark ? "Switch to Light Mode" : "Switch to Dark Mode");
+}
+
+function updateThemeOnHtmlEl({ theme }) {
+    document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+const button = document.querySelector("[data-theme-toggle]");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+button.addEventListener("click", (event) => {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+    localStorage.setItem("theme", newTheme);
+    updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+    updateThemeOnHtmlEl({ theme: newTheme });
+
+    currentThemeSetting = newTheme;
+}); 
